f621a20f900ee6ca0136a289d0f42f0828bf04a1
feffe1702 <fpm.asd@gmail.com>
2024-10-04 14:31:26 +0200

    verify condition

--- palindromo/script.js
+++ palindromo/script.js
@@ -1,14 +1,22 @@
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
-let parola;
 
 function palindromo(parola) {
-    parola = prompt("inserisci qui la tua parola");
-    console.log(parola);
     return parola.split('').reverse().join('');
 }
-let parolaReverse = palindromo()
+
+let parola;
+parola = prompt("inserisci qui la tua parola");
+console.log(parola);
+
+let parolaReverse = palindromo(parola);
 console.log(parolaReverse);
+
+if(parola == parolaReverse){
+    alert(`${parola} è un palindromo`);
+} else {
+    alert(`${parola} non è un palindromo`);
+}
 
 
 
